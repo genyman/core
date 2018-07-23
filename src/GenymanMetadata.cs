fa80bdde9c7bef424d9ea5c78496644893277edc
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2018-07-23 11:03:04 +0200

    Add some info to all configuration files

--- src/GenymanMetadata.cs
+++ src/GenymanMetadata.cs
@@ -19,6 +19,7 @@ namespace Genyman.Core
 		public string PackageId { get; set; }
 		public string Version { get; set; }
 		public string NugetSource { get; set; }
+		public string Info { get; } = "This is a Genyman configuration file - https://genyman.github.io/docs/";
 
 		internal string Identifier => PackageId.Split(".").Last();
 		internal string Description { get; set; }
