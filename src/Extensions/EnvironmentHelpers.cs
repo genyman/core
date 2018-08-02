deb9beb257ec23b668c24cdd21b7f2024b843f4d
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2018-08-02 16:50:43 +0200

    EnvironmentVar helper

---
+++ src/Extensions/EnvironmentHelpers.cs
@@ -0,0 +1,14 @@
+using System;
+
+// ReSharper disable once CheckNamespace
+namespace Genyman.Core
+{
+	public static class EnvironmentHelpers
+	{
+		public static string FromEnvironmentOrDefault(this string value)
+		{
+			if (value.StartsWith("$")) return Environment.GetEnvironmentVariable(value);
+			return value;
+		}
+	}
+}
