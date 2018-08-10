fb4ee42d5c7f6d8c5ed82fc421cc95d07e7c5b43
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2018-08-10 19:13:05 +0200

    Fix when processing non text files; DefaultSkippedExtensions static list added with default list of binary files

--- src/Handlebars/FluentHandlebars.cs
+++ src/Handlebars/FluentHandlebars.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using ServiceStack;
 
 namespace Genyman.Core.Handlebars
 {
@@ -11,6 +13,12 @@ namespace Genyman.Core.Handlebars
 		string _template;
 		object _model;
 		readonly object _caller;
+
+		public static readonly List<string> DefaultSkippedExtensions = new List<string>()
+		{
+			"png", "jpg", "jpeg", "gif", "bmp", "tif", "tiff",
+			"dll", "bin", "pbd"
+		};
 
 		FluentHandlebars(object caller)
 		{
@@ -24,6 +32,8 @@ namespace Genyman.Core.Handlebars
 		}
 
 		public HandlebarsDotNet.IHandlebars Instance => _handlebars;
+
+		internal bool Skip { get; set; }
 
 		public FluentHandlebars WithStringHelpers()
 		{
@@ -43,7 +53,7 @@ namespace Genyman.Core.Handlebars
 			_fluentInstance.WithCSharpHelpers();
 			return _fluentInstance;
 		}
-		
+
 		public FluentHandlebars WithCustomHelper(Action<HandlebarsDotNet.IHandlebars> customHelper)
 		{
 			customHelper.Invoke(_handlebars);
@@ -71,17 +81,36 @@ namespace Genyman.Core.Handlebars
 						stream = assembly.GetManifestResourceStream(resource);
 				}
 			}
+
 			using (var reader = new StreamReader(stream))
 			{
 				source = reader.ReadToEnd();
 			}
+
 			_template = source;
 			return _fluentInstance;
 		}
 
 		public FluentHandlebars UsingFileTemplate(string fileName)
 		{
-			_template = File.ReadAllText(fileName);
+			if (DefaultSkippedExtensions.Contains(fileName.GetExtension().Replace(".", "")))
+			{
+				_fluentInstance.Skip = true;
+				Log.Debug($"{fileName.GetExtension()} cannot be handled by Handlebars");
+			}
+			else
+			{
+				try
+				{
+					_template = File.ReadAllText(fileName);
+				}
+				catch (Exception)
+				{
+					Skip = true;
+					Log.Error($"Could not read {fileName}. Is this a text file? If not, you can add the extension to FluentHandlebars.DefaultSkippedExtensions list");
+				}
+			}
+
 			return _fluentInstance;
 		}
 
@@ -99,6 +128,8 @@ namespace Genyman.Core.Handlebars
 
 		public string OutputFile(string fileName, bool overwrite = false)
 		{
+			if (Skip) return null;
+
 			if (fileName.Contains("{{"))
 			{
 				// trick handlebars on Windows file names
@@ -108,26 +139,27 @@ namespace Genyman.Core.Handlebars
 					escapeAvailable = true;
 					fileName = fileName.Replace("\\", "/");
 				}
-				
+
 				fileName = FluentHandlebars.Create(this)
 					.HavingModel(_model)
 					.UsingTemplate(fileName)
 					.OutputString();
-				
+
 				if (escapeAvailable)
 					fileName = fileName.Replace("/", "\\");
 			}
-			
+
 			var result = OutputString();
 			if (!overwrite && File.Exists(fileName))
 			{
 				Log.Warning($"Skipping {fileName} - File already exists");
 				return fileName;
 			}
+
 			try
 			{
 				fileName.EnsureFolderExists();
-			    File.WriteAllText(fileName, result, System.Text.Encoding.UTF8);
+				File.WriteAllText(fileName, result, System.Text.Encoding.UTF8);
 				return fileName;
 			}
 			catch (Exception e)
